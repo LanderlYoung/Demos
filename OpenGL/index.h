39ef9618144fef7a0e8a83979b669b9c84f93a0c
taylorcyang <taylorcyang@tencent.com>
2019-07-10 19:37:34 +0800

    split samples

---
+++ OpenGL/index.h
@@ -0,0 +1,26 @@
+/**
+* <pre>
+* Author: taylorcyang@tencent.com
+* Date:   2019-07-10
+* Time:   18:00
+* Life with Passion, Code with Creativity.
+* </pre>
+*/
+
+#pragma once
+
+#include <iostream>
+#include <GLUT/glut.h>
+#include <cmath>
+
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+void lineSegment();
+
+void drawCircle();
+
+void drawSinWave();
+
+void polygonFace();
+
+void polygonStipple();
