b3024fce8330bfeda1fe5d1e8c36a680631b7524
taylorcyang <taylorcyang@tencent.com>
2019-09-17 16:48:04 +0800

    initial commit Android OpenGl Demo

---
+++ AndroidOpenGlTest/app/src/main/java/io/github/landerlyoung/androidopengltest/MyGlSurfaceView.java
@@ -0,0 +1,12 @@
+package io.github.landerlyoung.androidopengltest;
+
+/**
+ * <pre>
+ * Author: taylorcyang@tencent.com
+ * Date:   2019-09-17
+ * Time:   16:46
+ * Life with Passion, Code with Creativity.
+ * </pre>
+ */
+public class MyGlSurfaceView {
+}
