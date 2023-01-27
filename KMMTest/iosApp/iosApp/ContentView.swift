fe1908bdbd3483c2fd8fa74a78213faa51cfb916
yangchao12 <yangchao12@kuaishou.com>
2023-01-27 13:09:14 +0800

    add KMM demo by https://kotlinlang.org/docs/multiplatform-mobile-dependencies.html#add-a-multiplatform-dependency -- add date time -- add network for Android & iOS -- change iOS prefix from shared to Kt by change framework base name -- add iOS Objective-C bridge -- add Objective-C dealloc test -- add Objective-C dealloc benchmark

--- KMMTest/iosApp/iosApp/ContentView.swift
+++ KMMTest/iosApp/iosApp/ContentView.swift
@@ -13,6 +13,7 @@ extension ContentView {
     class ViewModel: ObservableObject {
         @Published var text = "Loading..."
             init() {
+                KMMBridge.mmBenchmark()
                 KMMBridge.getLastSuccessfulLaunch { greeting, error in
                     if let greeting = greeting {
                         self.text = greeting
