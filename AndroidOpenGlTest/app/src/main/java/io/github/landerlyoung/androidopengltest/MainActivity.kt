78274d4d84157b80b436a1cee68a6c4579bce39f
landerlyoung <landerlyoung@gmail.com>
2024-10-12 17:38:45 +0800

    sth

--- AndroidOpenGlTest/app/src/main/java/io/github/landerlyoung/androidopengltest/MainActivity.kt
+++ AndroidOpenGlTest/app/src/main/java/io/github/landerlyoung/androidopengltest/MainActivity.kt
@@ -1,7 +1,9 @@
 package io.github.landerlyoung.androidopengltest
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +14,21 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
-        my_gl_surface_view.renderer = {
-//            GLES20.glClearColor(1f, 0f,0f,1f)
-//            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-            render()
+//        my_gl_surface_view.renderer = { w, h ->
+////            GLES20.glClearColor(1f, 0f,0f,1f)
+////            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+//            render(w, h)
+//        }
+        my_gl_surface_view.setOnClickListener {
+            val lp = my_gl_surface_view.layoutParams as ConstraintLayout.LayoutParams
+            lp.matchConstraintPercentHeight -= 0.2f
+            if (lp.matchConstraintPercentHeight < 0.2f) {
+                lp.matchConstraintPercentHeight = 1f
+            }
+            my_gl_surface_view.requestLayout()
+        }
+        my_gl_surface_view.let {
+            it.setBackgroundColor(Color.BLUE)
         }
     }
 
@@ -25,7 +38,7 @@ class MainActivity : AppCompatActivity() {
      */
     external fun stringFromJNI(): String
 
-    external fun render()
+    external fun render(width: Int, height: Int)
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
