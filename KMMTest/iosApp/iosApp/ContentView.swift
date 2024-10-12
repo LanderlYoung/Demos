73115af7142b4cd046806441fc52e1fbe4783d37
landerlyoung <landerlyoung@gmail.com>
2024-10-12 17:38:45 +0800

    add KMM demo by https://kotlinlang.org/docs/multiplatform-mobile-dependencies.html#add-a-multiplatform-dependency -- add date time -- add network for Android & iOS -- change iOS prefix from shared to Kt by change framework base name -- add iOS Objective-C bridge

--- KMMTest/iosApp/iosApp/ContentView.swift
+++ KMMTest/iosApp/iosApp/ContentView.swift
@@ -13,13 +13,11 @@ extension ContentView {
     class ViewModel: ObservableObject {
         @Published var text = "Loading..."
             init() {
-                Greeting().greeting { greeting, error in
-                    DispatchQueue.main.async {
-                        if let greeting = greeting {
-                            self.text = greeting
-                        } else {
-                            self.text = error?.localizedDescription ?? "error"
-                        }
+                KMMBridge.getLastSuccessfulLaunch { greeting, error in
+                    if let greeting = greeting {
+                        self.text = greeting
+                    } else {
+                        self.text = error?.localizedDescription ?? "error"
                     }
                 }
             }
---
+++ KMMTest/iosApp/iosApp/KMMBridge.h
@@ -0,0 +1,19 @@
+//
+//  KotlinObjcBridge.h
+//  iosApp
+//
+//  Created by landerlyoung on 2023/1/27.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KMMBridge : NSObject
+
++ (void)getLastSuccessfulLaunchWithCallback:(void(^)(NSString * _Nullable, NSError * _Nullable)) callback;
+
+@end
+
+NS_ASSUME_NONNULL_END
