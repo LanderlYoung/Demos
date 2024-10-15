9f12a3542ca66b5f1b20463e876281a75131c768
landerlyoung <landerlyoung@gmail.com>
2024-10-15 17:13:00 +0800

    add notification

--- tauri-test-npm/src/App.jsx
+++ tauri-test-npm/src/App.jsx
@@ -4,9 +4,8 @@ import { invoke } from "@tauri-apps/api/core";
 import { message, } from '@tauri-apps/plugin-dialog';
 import { Window } from "@tauri-apps/api/window"
 import { Webview } from "@tauri-apps/api/webview"
-import { TauriEvent } from "@tauri-apps/api/event";
+import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/plugin-notification';
 import "./App.css";
-import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -21,38 +20,6 @@ function App() {
       kind: 'warning',
       okLabel: 'Great'
     });
-  }
-
-  async function logEvent(who, name) {
-    await who.once(name, e => {
-      console.log(`${name} event: ${JSON.stringify(e)}`);
-    });
-  }
-
-  async function openNewWindow(label, url) {
-    // TODO: tauri window/webview JS API not working properly
-    // https://tauri.app/reference/javascript/api/namespacewebview/
-    console.log("open webview " + url);
-
-    const appWindow = new Window(label);
-    const webview = new Webview(appWindow, label, {
-      url: url,
-      x: 0,
-      y: 0,
-      width: 800,
-      height: 600,
-    })
-    await logEvent(webview, 'tauri://created');
-    await logEvent(webview, 'tauri://error');
-    console.log("created webview " + webview + " for url " + url);
-
-    appWindow.once('tauri://created', e => {
-      appWindow.setTitle('Test second window');
-      setTimeout(() => {
-        console.log("close window " + appWindow.label + " for url " + url);
-        appWindow.close();
-      }, 5000);
-    })
   }
 
   return (
@@ -94,6 +61,53 @@ function App() {
       <p>{greetMsg}</p>
     </div>
   );
+}
+
+async function logEvent(who, name) {
+  await who.once(name, e => {
+    console.log(`${name} event: ${JSON.stringify(e)}`);
+  });
+}
+
+async function openNewWindow(label, url) {
+  // TODO: tauri window/webview JS API not working properly
+  // https://tauri.app/reference/javascript/api/namespacewebview/
+  console.log("open webview " + url);
+
+  await postNotification("open webview " + url);
+
+  const appWindow = new Window(label);
+  const webview = new Webview(appWindow, label, {
+    url: url,
+    x: 0,
+    y: 0,
+    width: 800,
+    height: 600,
+  })
+  await logEvent(webview, 'tauri://created');
+  await logEvent(webview, 'tauri://error');
+  console.log("created webview " + webview + " for url " + url);
+
+  appWindow.once('tauri://created', e => {
+    appWindow.setTitle('Test second window');
+    setTimeout(() => {
+      console.log("close window " + appWindow.label + " for url " + url);
+      appWindow.close();
+    }, 5000);
+  })
+}
+
+let needRequestPermission = true;
+async function postNotification(body, title) {
+  let permissionGranted = await isPermissionGranted();
+  if (!permissionGranted && needRequestPermission) {
+    needRequestPermission = false;
+    const permission = await requestPermission();
+    permissionGranted = permission === 'granted';
+  }
+  if (permissionGranted) {
+    sendNotification({ title: title, body: body });
+  }
 }
 
 export default App;
